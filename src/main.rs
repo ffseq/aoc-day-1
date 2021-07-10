2289423919b3546218d997383f8fbfb3b5d915f1
Alex Lorello <ffseq@protonmail.com>
2021-07-09 20:35:39 -0400

    Modified for Part 2

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main() {
     
     let numbers = parse_input("input.txt");
 
-    let (a, b, ab) = find_nums(&numbers);
-    println!("Ans: {} * {} is {}", a, b, ab);
+    let (a, b, c, abc) = find_nums(&numbers);
+    println!("Ans: {} * {} * {} is {}", a, b, c, abc);
 }
 
 fn parse_input(inputfile: &str) -> Vec<i32> {
@@ -26,16 +26,18 @@ fn parse_input(inputfile: &str) -> Vec<i32> {
     numbers
 }    
 
-fn find_nums(v: &Vec<i32>) -> (i32, i32, i32) {
+fn find_nums(v: &Vec<i32>) -> (i32, i32, i32, i32) {
     const TARGET: i32 = 2020;
 
     for i in 0..v.len()  {
         for j in i..v.len() {
-            if v[i] + v[j] == TARGET {
-                return (v[i], v[j], v[i] * v[j]);
+            for k in j..v.len() {
+                if v[i] + v[j] + v[k] == TARGET {
+                    return (v[i], v[j], v[k], v[i] * v[j] * v[k]);
+                }
             }
         }
     }
 
-    (0, 0, 0)
+    (0, 0, 0, 0)
 }
